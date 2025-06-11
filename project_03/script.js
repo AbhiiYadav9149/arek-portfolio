85cfc0604eec059155350b7f892a5cdcdcb667b3
Abhishek Yadav <akabhishekyadav1114@gmail.com>
2025-06-11 23:45:59 +0530

    Updates

---
+++ project_03/script.js
@@ -0,0 +1,9 @@
+
+
+function open_nav() {
+    document.getElementById('nav').style.translate = '-100%';
+}
+
+function close_nav() {
+    document.getElementById('nav').style.translate = '100%';
+}
---
+++ project_08/script.js
@@ -0,0 +1,8 @@
+function open_nav() {
+    document.getElementById('nav').style.translate = "100%";
+}
+
+
+function close_nav() {
+    document.getElementById('nav').style.translate = "-100%";
+}
